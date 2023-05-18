3ff011f75a6c0bccdbe681d2a6ff25d044e4de4a
BalaAtchaya2004 <117425696+BalaAtchaya2004@users.noreply.github.com>
2023-05-18 21:24:12 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL:  "http://localhost:5000",
+  baseURL:  "https://stackoverflowclone-mern.vercel.app/",
 });
 
 API.interceptors.request.use((req) => {

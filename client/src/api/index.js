65a68c4f53095037104ca38ab23b226a6f6cdcf1
BalaAtchaya2004 <117425696+BalaAtchaya2004@users.noreply.github.com>
2023-05-19 16:11:21 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 
-const API = axios.create({
-  baseURL:  "https://mern-backend-lh2f.onrender.com/",
-});
+const API = axios.create({ baseURL: "http://localhost:5000" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("Profile")) {

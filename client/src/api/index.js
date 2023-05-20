f6e56886bb54a941c60ed95bec575f39491ac149
BalaAtchaya2004 <117425696+BalaAtchaya2004@users.noreply.github.com>
2023-05-20 12:46:34 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({ 
-  baseURL: "https://stackoverflow-mern-aj.onrender.com",
+  baseURL: "http://localhost:5000",
 });
 
 API.interceptors.request.use((req) => {

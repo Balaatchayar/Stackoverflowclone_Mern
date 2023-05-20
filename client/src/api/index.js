c2ae5ae0e0f6158d659708562abb0d61110f926c
BalaAtchaya2004 <117425696+BalaAtchaya2004@users.noreply.github.com>
2023-05-20 14:13:22 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-  baseURL: "https://mernproject-78yn.onrender.com",
+  baseURL: "https://mernproject-78yn.onrender.com/",
 });
 
 API.interceptors.request.use((req) => {
